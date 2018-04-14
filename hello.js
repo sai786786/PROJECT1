384d4e71d0babe9ff7c215261f9ca5d6a73ab5b5
sai786786 <33508924+sai786786@users.noreply.github.com>
2018-04-14 15:34:49 +0000

    Update hello.js

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@
         res.writeHead(200, {
             'Content-Type': 'text/plain'
         });
-        res.end('Hello Jenkins is working test 20:50!!!');
+        res.end('Hello Jenkins is working test 20:50,21:04!!!');
     });
      
     server.listen(3000);
