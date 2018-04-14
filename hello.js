19b4dbd29375d65521185e0c578b8798065f061f
sai786786 <33508924+sai786786@users.noreply.github.com>
2018-04-14 09:49:39 +0000

    Update hello.js

--- hello.js
+++ hello.js
@@ -4,8 +4,8 @@
         res.writeHead(200, {
             'Content-Type': 'text/plain'
         });
-        res.end('Hello!!!');
+        res.end('Hello Jenkins is working!!!');
     });
      
-    server.listen(3001);
+    server.listen(3000);
     console.log('Server running at http://127.0.0.1:3000/');
